cd326a1152834be10bd6047c7040d7bfd98ce42c
Cagan Sevencan <cagansvncn@gmail.com>
2019-03-26 17:50:46 -0700

    Added

---
+++ InstaParse/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  InstaParse
+//
+//  Created by Cagan Sevencan on 3/25/19.
+//  Copyright © 2019 Cagan Sevencan. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
